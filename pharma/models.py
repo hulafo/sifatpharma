a7f1bbca47d61eb1d4f3e92c2675deb6690d9ea2
hulafo <hulafo1989@gmail.com>
2017-09-26 08:10:25 +0800

    new template

--- pharma/models.py
+++ pharma/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from ckeditor.fields import RichTextField
+#from ckeditor.fields import RichTextField
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=140)
-    shortbd=RichTextField()
-    body=RichTextField()
+    shortbd=models.TextField()
+    body=models.TextField()
     image=models.ImageField(upload_to='blog/%Y/%m/%d/',blank=True)
     date=models.DateTimeField()
     
